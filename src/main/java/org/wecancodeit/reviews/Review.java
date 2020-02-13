3bf897474d768cc7a0aae53f95c58240ecff3106
Gaelan Shively <gaelanshively@gmail.com>
2020-02-13 13:48:13 -0500

    HTTP Request Test added and failed

---
+++ src/main/java/org/wecancodeit/reviews/Review.java
@@ -0,0 +1,41 @@
+package org.wecancodeit.reviews;
+
+import java.util.Date;
+
+public class Review {
+    private String reviewAuthor;
+    private int reviewRating;
+    private Date reviewDate;
+    private String reviewText;
+    private String reviewHashTags;
+
+    public Review(String reviewAuthor, int reviewRating, Date reviewDate, String reviewText, String reviewHashTags) {
+        this.reviewAuthor = reviewAuthor;
+        this.reviewRating = reviewRating;
+        this.reviewDate = reviewDate;
+        this.reviewText = reviewText;
+        this.reviewHashTags = reviewHashTags;
+    }
+
+
+    public String getReviewAuthor() {
+        return reviewAuthor;
+    }
+
+    public int getReviewRating() {
+        return reviewRating;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public String getReviewHashTags() {
+        return reviewHashTags;
+    }
+
+}
