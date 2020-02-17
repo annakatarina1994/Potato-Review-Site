640c346103682f4696f78f3ec2c157f355f3b2c9
daly <dalyeu@yahoo.fr>
2020-02-17 09:59:04 -0500

    added smokeTest

---
+++ src/test/java/org/wecancodeit/reviews/SmokeTest.java
@@ -0,0 +1,14 @@
+package org.wecancodeit.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+
+}
