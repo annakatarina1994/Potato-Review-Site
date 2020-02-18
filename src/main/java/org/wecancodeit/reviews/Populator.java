481c28811bd384c6ad5d0e1184f334e45188435e
daly <dalyeu@yahoo.fr>
2020-02-18 13:12:15 -0500

    added the populator Class

---
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -0,0 +1,28 @@
+package org.wecancodeit.reviews;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Populator implements CommandLineRunner {
+    @Autowired
+    MapListStorage categoryStorage;
+
+    public Populator(MapListStorage categoryStorage){
+        this.categoryStorage = categoryStorage;
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Populator runnig...");
+
+        Category russetCategory = new Category("Russet","Russet description");
+        Category fingerlingCategory = new Category("Fingerling","Fingerling description");
+        Category yellowCategory = new Category("Yellow","I'am yellow");
+        Category whiteCategory = new Category("White","It is white");
+        categoryStorage.storeCategory(russetCategory);
+        categoryStorage.storeCategory(yellowCategory);
+        categoryStorage.storeCategory(whiteCategory);
+        categoryStorage.storeCategory(fingerlingCategory);
+       }
+}
