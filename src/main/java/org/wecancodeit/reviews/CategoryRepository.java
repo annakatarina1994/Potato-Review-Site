be083810dec96be83f4ef3d1a3037c121f7ed79e
Anna Lizon <annakatarina1994@gmail.com>
2020-02-18 14:55:40 -0500

    worked on populator, some styling stuff

--- src/main/java/org/wecancodeit/reviews/CategoryRepository.java
+++ src/main/java/org/wecancodeit/reviews/CategoryRepository.java
@@ -1,10 +1,12 @@
 package org.wecancodeit.reviews;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     Optional<Category> findByName(String categoryToFind);
--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -1,28 +1,21 @@
 package org.wecancodeit.reviews;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Populator implements CommandLineRunner {
-    @Autowired
-    MapCategoryStorage categoryStorage;
 
-    public Populator(MapCategoryStorage categoryStorage){
+    CategoryStorage categoryStorage;
+
+    public Populator(CategoryStorage categoryStorage){
         this.categoryStorage = categoryStorage;
     }
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Populator runnig...");
+        System.out.println("Populator running...");
 
-        Category russetCategory = new Category("Russet","Russet description");
-        Category fingerlingCategory = new Category("Fingerling","Fingerling description");
-        Category yellowCategory = new Category("Yellow","I'am yellow");
-        Category whiteCategory = new Category("White","It is white");
-        categoryStorage.storeCategory(russetCategory);
-        categoryStorage.storeCategory(yellowCategory);
-        categoryStorage.storeCategory(whiteCategory);
-        categoryStorage.storeCategory(fingerlingCategory);
+        categoryStorage.storeCategory(new Category("Russet", "this is a description"));
+        categoryStorage.storeCategory(new Category("White", "This is also a description"));
        }
 }
