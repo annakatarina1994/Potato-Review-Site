af32b1ecc548b3609846b5d5072a3162cb981e15
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-02-20 10:32:13 -0500

    updated categories

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -16,9 +16,17 @@ public class Populator implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Populator running...");
 
-        Category russet = new Category("Russet", "this is a description");
+        Category russet = new Category("Russet", "A large potato with dark brown skin and few eyes");
         categoryStorage.storeCategory(russet);
-        Category white = new Category("White", "This is also a description");
+        Category white = new Category("White", "A potato with smooth skin, few eyes and a bright white appearance");
         categoryStorage.storeCategory(white);
+        Category red = new Category("Red", "A smaller potato with deep red skin and light brown markings");
+        categoryStorage.storeCategory(red);
+        Category blue  = new Category("Blue", "A large and oblong potato with blue skin and purple flesh");
+        categoryStorage.storeCategory(blue);
+        Category yellow  =  new Category("Yellow", "A potato with skin and flesh that is yellow or golden");
+        categoryStorage.storeCategory(yellow);
+        Category fingerling = new Category("Fingerling", "A small finger shaped potato that grows small and narrow");
+        categoryStorage.storeCategory(fingerling);
        }
 }
