cbcb1e6be3519bbb7c4fb7374a5f7f35cca42628
Anna Lizon <annakatarina1994@gmail.com>
2020-04-15 16:07:04 -0400

    first commit in personal repo

--- src/main/java/org/wecancodeit/reviews/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CategoryController {
 
     private CategoryStorage categoryStorage;
-
     private ReviewStorage reviewStorage;
 
     public CategoryController(CategoryStorage categoryStorage,ReviewStorage reviewStorage) {
--- src/main/java/org/wecancodeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancodeit/reviews/ReviewRepository.java
@@ -6,5 +6,4 @@ import java.util.Optional;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 Optional<Review> findById(Long reviewToFind);
-
 }
